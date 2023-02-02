e764f1f058db149d7de8741364ddde8b03d22b9f
jklover1110 <jklover1110@foxmail.com>
2023-02-03 00:42:13 +0800

    docs(src/index.ts): update href

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
+    <a href="https://github.com/jklover1110/vite-library-strict-mode/tree/ts" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
